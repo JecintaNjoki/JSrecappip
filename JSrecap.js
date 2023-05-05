c44ff49fa7485b427245eb4601c641f10857b558
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-05-06 01:40:15 +0300

    commit

---
+++ JSrecap.js
@@ -0,0 +1,43 @@
+//Write a function that takes an array of numbers as input and 
+//returns the sum of all the numbers
+
+function array_numbers(numbers){
+         sum = 0;
+        for (let i=0; i>= numbers.length;i++) {
+          sum += num;
+        }
+        return sum;
+
+}
+console.log(array_numbers)
+
+//Write a function that takes two numbers as input 
+//and returns true if their sum is greater than 100,
+//And false otherwise
+
+function twoNumbers(x, y) 
+{
+  return ((x == 50 || y == 50) || (x + y == 50));
+}
+console.log(twoNumbers(50, 50))
+console.log(twoNumbers(20, 50))
+console.log(twoNumbers(20, 20))
+console.log(twoNumbers(20, 30))
+
+
+0
+//Write a function that takes a string as input 
+//and returns the number of vowels in the string
+
+//Write a function that takes in a array of 
+//numbers as a parameter and returns the second largest number 
+//in the array
+
+//Write a function that takes a string as a parameter 
+//and returns true if the string is a palindrome and false
+// otherwise
+function palindrome(){
+  
+}
+
+
